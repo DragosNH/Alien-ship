6a842b7a0a5d562592af6dd6efc0bfa8c8f10b8e
Dragoş <edragos33@gmail.com>
2025-07-31 11:32:21 +0200

    innew windshield added

--- script.js
+++ script.js
@@ -17,14 +17,20 @@ camera.position.z = 5;
 // --- Windshield ---
 const windshieldGeo = new THREE.CapsuleGeometry(1.5, 1, 32, 10);
 const windshieldMat = new THREE.MeshBasicMaterial({
-    color: 0x6e9349
+    color: 0x6e9349,
+    opacity: 0.8
 });
 const windshield = new THREE.Mesh(windshieldGeo, windshieldMat);
-
-// --- Plate side ---
+// -- inner windshield --
+const innerWindshieldGeo = new THREE.CapsuleGeometry(1.2, 1, 32, 10);
+const innerWindshieldMat = new THREE.MeshBasicMaterial({
+    color: 0x587040
+});
+const innerWindshield = new THREE.Mesh(innerWindshieldGeo, innerWindshieldMat);
+// --- Plates ---
 // -- Superiour plate --
 const superiourPlateGeo = new THREE.TorusGeometry(2, 0.7, 3, 80);
-const superiourPlateMat = new THREE.MeshNormalMaterial({
+const superiourPlateMat = new THREE.MeshBasicMaterial({
     color: 0xb9bbbe
 });
 const superiourPlate = new THREE.Mesh(superiourPlateGeo, superiourPlateMat);
@@ -32,7 +38,7 @@ superiourPlate.rotation.x = 4.7;
 superiourPlate.position.y -= 0.8;
 // -- Middle Plate --
 const middlePlateGeo = new THREE.TorusGeometry(1.2, 0.3, 3, 80);
-const middlePlateMat = new THREE.MeshNormalMaterial({
+const middlePlateMat = new THREE.MeshBasicMaterial({
     color: 0x939599
 });
 const middlePlate = new THREE.Mesh(middlePlateGeo, middlePlateMat);
@@ -40,7 +46,7 @@ middlePlate.rotation.x = 4.7;
 middlePlate.position.y -= 1.5;
 // -- inferiour Plate --
 const inferiourPlateGeo = new THREE.TorusGeometry(1, 0.1, 3, 80);
-const inferiourPlateMat = new THREE.MeshNormalMaterial({
+const inferiourPlateMat = new THREE.MeshBasicMaterial({
     color: 0x75777b
 });
 const inferiourPlate = new THREE.Mesh(inferiourPlateGeo, inferiourPlateMat);
@@ -50,6 +56,7 @@ inferiourPlate.position.y -= 2;
 
 // Objects added to the scene
 scene.add(windshield);
+scene.add(innerWindshield);
 scene.add(superiourPlate);
 scene.add(middlePlate);
 scene.add(inferiourPlate);
