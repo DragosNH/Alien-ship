0395eccd84228f8e24f636271fa8b62d3ccee02a
Dragoş <edragos33@gmail.com>
2025-07-31 10:56:18 +0200

    shape finished

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // ------ Scene and camera ------
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // ------ Renderer ------
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 camera.position.z = 5;
 
 // ------ Geometry ------
-const windshieldGeo = new THREE.CapsuleGeometry(1, 1, 4, 8);
+// --- Windshield ---
+const windshieldGeo = new THREE.CapsuleGeometry(1.5, 1, 32, 10);
 const windshieldMat = new THREE.MeshBasicMaterial({
     color: 0x6e9349
 });
 const windshield = new THREE.Mesh(windshieldGeo, windshieldMat);
 
+// --- Plate side ---
+// -- Superiour plate --
+const superiourPlateGeo = new THREE.TorusGeometry(2, 0.7, 3, 80);
+const superiourPlateMat = new THREE.MeshNormalMaterial({
+    color: 0xb9bbbe
+});
+const superiourPlate = new THREE.Mesh(superiourPlateGeo, superiourPlateMat);
+superiourPlate.rotation.x = 4.7;
+superiourPlate.position.y -= 0.8;
+// -- Middle Plate --
+const middlePlateGeo = new THREE.TorusGeometry(1.2, 0.3, 3, 80);
+const middlePlateMat = new THREE.MeshNormalMaterial({
+    color: 0x939599
+});
+const middlePlate = new THREE.Mesh(middlePlateGeo, middlePlateMat);
+middlePlate.rotation.x = 4.7;
+middlePlate.position.y -= 1.5;
+// -- inferiour Plate --
+const inferiourPlateGeo = new THREE.TorusGeometry(1, 0.1, 3, 80);
+const inferiourPlateMat = new THREE.MeshNormalMaterial({
+    color: 0x75777b
+});
+const inferiourPlate = new THREE.Mesh(inferiourPlateGeo, inferiourPlateMat);
+inferiourPlate.rotation.x = 4.7;
+inferiourPlate.position.y -= 2;
 
 
+// Objects added to the scene
 scene.add(windshield);
-
+scene.add(superiourPlate);
+scene.add(middlePlate);
+scene.add(inferiourPlate);
 
 
 
 // ------ Responsive page ------
-window.addEventListener('resize', ()=>{
+window.addEventListener('resize', () => {
     const width = window.innerWidth;
     const height = window.innerHeight;
 
@@ -38,6 +69,8 @@ window.addEventListener('resize', ()=>{
 
 // ------ Animate ------
 function animate() {
-  renderer.render( scene, camera );
+
+    controls.update();
+    renderer.render(scene, camera);
 }
-renderer.setAnimationLoop( animate );
+renderer.setAnimationLoop(animate);
