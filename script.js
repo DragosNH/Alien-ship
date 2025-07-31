0194ff7f0ee64b1599ad84b6c1b465d06e4d025e
Dragoş <edragos33@gmail.com>
2025-07-31 10:27:36 +0200

    hello cube + responsive design added

---
+++ script.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+
+// ------ Scene and camera ------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// ------ Renderer ------
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 5;
+
+// ------ Geometry ------
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+
+
+
+// ------ Responsive page ------
+window.addEventListener('resize', ()=>{
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+});
+
+// ------ Animate ------
+function animate() {
+  renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
